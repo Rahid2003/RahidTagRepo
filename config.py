8a02c387d361821363595e13c2fe7b528af76fda
Rahid2003 <104177696+Rahid2003@users.noreply.github.com>
2022-12-04 05:09:05 +0400

    update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ from telethon import TelegramClient, events
 # Buranı qurdalama
 # Yalnız deploy buttonuyla botunu yarat
 # 
-api_id = int(os.environ.get("APP_ID"))
-api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("TOKEN")
+api_id = int(os.environ.get("APP_ID", "19485442"))
+api_hash = os.environ.get("API_HASH", "a03fcb372b3ec4e406b5d52f84b02e53")
+bot_token = os.environ.get("TOKEN", "5394785524:AAGiGd4Xth_0Fa4oe32FYWEAfbTVbeD_W4M")
 # Telethon 
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 #
